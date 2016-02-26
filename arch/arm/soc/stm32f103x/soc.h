b72823edec53c2f6a487df2077303c9b14854652
Maciej Borzecki <maciej.borzecki@open-rnd.pl>
2016-02-26 16:28:09 +0100

    stm32f103x: add base addresses for GPIO ports
    Signed-off-by: Maciej Borzecki <maciej.borzecki@open-rnd.pl>

--- arch/arm/soc/stm32f103x/soc.h
+++ arch/arm/soc/stm32f103x/soc.h
@@ -37,6 +37,14 @@
 /* Reset and Clock Control */
 #define RCC_BASE              (AHBPERIPH_BASE + 0x1000)
 
+#define GPIOA_BASE            (APB2PERIPH_BASE + 0x0800)
+#define GPIOB_BASE            (APB2PERIPH_BASE + 0xC000)
+#define GPIOC_BASE            (APB2PERIPH_BASE + 0x1000)
+#define GPIOD_BASE            (APB2PERIPH_BASE + 0x1400)
+#define GPIOE_BASE            (APB2PERIPH_BASE + 0x1800)
+#define GPIOF_BASE            (APB2PERIPH_BASE + 0x1C00)
+#define GPIOG_BASE            (APB2PERIPH_BASE + 0x2000)
+
 #ifndef _ASMLANGUAGE
 
 #include <device.h>

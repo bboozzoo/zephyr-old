099c8106cd76300e0e7f9480975c06d29ace28cb
Maciek Borzecki <maciek.borzecki@gmail.com>
2016-02-27 22:09:34 +0100

    stm32f103x/gpio: document where register maps come from
    Signed-off-by: Maciej Borzecki <maciej.borzecki@open-rnd.pl>

--- arch/arm/soc/stm32f103x/gpio.h
+++ arch/arm/soc/stm32f103x/gpio.h
@@ -1,6 +1,18 @@
 #ifndef _STM32F103_GPIO_H_
 #define _STM32F103_GPIO_H_
 
+/**
+ * @brief
+ *
+ * Based on reference manual:
+ *   STM32F101xx, STM32F102xx, STM32F103xx, STM32F105xx and STM32F107xx
+ *   advanced ARM ® -based 32-bit MCUs
+ *
+ * Chapter 9: General-purpose and alternate-function I/Os
+ *            (GPIOs and AFIOs)
+ */
+
+/* 9.2 GPIO registers */
 struct stm32f10x_gpio
 {
 	uint32_t crl;

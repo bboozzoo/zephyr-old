e697a60750114c39958ff1706ef0c20c21867fb8
Maciek Borzecki <maciek.borzecki@gmail.com>
2016-02-29 21:46:53 +0100

    soc/stm32f103x: document the source of memory map defines
    Signed-off-by: Maciej Borzecki <maciek.borzecki@gmail.com>

--- arch/arm/soc/stm32f103x/soc.h
+++ arch/arm/soc/stm32f103x/soc.h
@@ -16,7 +16,14 @@
 
 /**
  * @file SoC configuration macros for the STM32F103 family processors.
+ *
+ * Based on reference manual:
+ *   STM32F101xx, STM32F102xx, STM32F103xx, STM32F105xx and STM32F107xx
+ *   advanced ARM ® -based 32-bit MCUs
+ *
+ * Chapter 3.3: Memory Map
  */
+
 
 #ifndef _STM32F103_SOC_H_
 #define _STM32F103_SOC_H_

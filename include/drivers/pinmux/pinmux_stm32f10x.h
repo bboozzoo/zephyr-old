42e1f3e8aa110e9f7bf13d715782c432f86bca22
Maciek Borzecki <maciek.borzecki@gmail.com>
2016-03-03 15:33:15 +0100

    pinmux/stm32f10x: add driver for STM32F1 pinmux
    Add driver for pin control subsystem in STM32F1 MCU series. The driver
    allows for pin mode configuration without the ability of pin remapping.
    
    The driver implements a pinmux driver API, with custom `func` and `pin`
    encoding in API calls. The caller is expected to use STM32PIN() helper
    macro for encoding port/pin numbers.
    
    While the driver allows for configuration of IO pin mode, the explicit
    configuration of alternate function is not supported. The usual practice
    is to consult the reference manual or STM32Cube for information on pin
    configuration required for particular mode. The driver assumes that user
    performs such action.
    
    Whenever an IO pin is being enabled, the driver will automatically
    enable the clock for corresponding port. The driver does not implement
    disabling of port's clock as this has potentially disruptive, as such
    such operation should be done explicitly in the code.
    
    The pin control module needs to be initialized before any other modules,
    but after clock_control. For this reason, the driver is initialized at
    PRIMARY level, with priority set to 2.
    
    Change-Id: I8cb746d0f3cad72cd50b3355fe6d93a9f469be25
    Signed-off-by: Maciej Borzecki <maciek.borzecki@gmail.com>

---
+++ include/drivers/pinmux/pinmux_stm32f10x.h
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) 2016 Open-RnD Sp. z o.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef __PINMUX_STM32F10X_H__
+#define __PINMUX_STM32F10X_H__
+
+#include <soc.h>
+
+/* pin functions */
+enum pin_config_mode {
+	PIN_CONFIG_BIAS_HIGH_IMPEDANCE,
+	PIN_CONFIG_BIAS_PULL_UP,
+	PIN_CONFIG_BIAS_PULL_DOWN,
+	PIN_CONFIG_ANALOG,
+	PIN_CONFIG_DRIVE_OPEN_DRAIN,
+	PIN_CONFIG_DRIVE_PUSH_PULL,
+	PIN_CONFIG_AF_PUSH_PULL,
+	PIN_CONFIG_AF_OPEN_DRAIN,
+};
+
+#endif /* __PINMUX_STM32F10X_H__ */

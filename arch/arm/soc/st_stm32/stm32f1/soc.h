ceb512510aad9d70f00a0dfb9490af2d86570778
Maciek Borzecki <maciek.borzecki@gmail.com>
2016-03-03 15:33:09 +0100

    soc/stm32f1: add GPIO registers mapping
    Add structure that maps GPIO port registers. The definition is added at
    SoC level to avoid duplication if as it will be accessed by multiple
    STM32F1 specific drivers.
    
    The patch also defines a couple of helper symbols for encoding a IO port
    pins into a convenient numerical format.
    
    Change-Id: I284fa35b243f44776125f7a9f7e08069fbdd8cce
    Origin: Original
    Signed-off-by: Maciej Borzecki <maciek.borzecki@gmail.com>

--- arch/arm/soc/st_stm32/stm32f1/soc.h
+++ arch/arm/soc/st_stm32/stm32f1/soc.h
@@ -61,6 +61,24 @@
 #include <misc/util.h>
 #include <drivers/rand32.h>
 
+/**
+ * @brief numerical IDs for IO ports
+ */
+enum stm32f10x_pin_port {
+	STM32_PORTA = 0,	/* IO port A */
+	STM32_PORTB = 1,	/* .. */
+	STM32_PORTC = 2,
+	STM32_PORTD = 3,
+	STM32_PORTE = 4,	/* IO port E */
+	STM32_PORTS_MAX,
+};
+
+/**
+ * @brief helper macro to encode an IO port pin in a numerical format
+ */
+#define STM32PIN(_port, _pin) \
+	(_port << 4 | _pin)
+
 /* clock subsystems */
 enum {
 	/* APB1 */

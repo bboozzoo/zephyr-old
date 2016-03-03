bcca6d02063b1c90748d83fa75bb81a169eebedd
Maciek Borzecki <maciek.borzecki@gmail.com>
2016-03-03 15:33:15 +0100

    pinmux/stm32: add common driver for STM32 pinmux
    Add a common driver for pin control subsystem in STM32 MCU series. The
    driver allows for pin mode configuration without the ability of pin
    remapping.
    
    The driver implements a pinmux driver API, with custom `func` and `pin`
    encoding in API calls. The caller is expected to use STM32PIN() helper
    macro for encoding port/pin numbers or using one of the provided
    STM32_PIN_* defines.
    
    The common driver requires SoC specific support to be implemented. The
    SoC code must implement these calls: stm32_get_pin_config(),
    stm32_get_port_clock(), stm32_pin_configure(). Consult pinmux_stm32.h
    header for detailn on semantics of these calls.
    
    Whenever an IO pin is being enabled, the driver will automatically
    enable the clock for corresponding port. The driver does not implement
    disabling of port's clock as this has potentially disruptive, as such
    such operation should be done explicitly in the code.
    
    The pin control module needs to be initialized before any other modules,
    but after clock_control. For this reason, the driver is initialized by
    default at PRIMARY level, with priority set to 2. The priority can be
    changed through configuration.
    
    Change-Id: I8cb746d0f3cad72cd50b3355fe6d93a9f469be25
    Origin: Original
    Signed-off-by: Maciej Borzecki <maciek.borzecki@gmail.com>

---
+++ drivers/pinmux/pinmux_stm32.c
@@ -0,0 +1,108 @@
+/*
+ * Copyright (c) 2016 Open-RnD Sp. z o.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * @brief
+ *
+ * A common driver for STM32 pinmux. Each SoC must implement a SoC
+ * specific part of the driver.
+ */
+
+#include <nanokernel.h>
+#include <device.h>
+#include <soc.h>
+#include "pinmux.h"
+#include <pinmux.h>
+#include <pinmux/pinmux_stm32.h>
+#include <clock_control/stm32_clock_control.h>
+
+/**
+ * @brief enable IO port clock
+ */
+static uint32_t enable_port(uint32_t port)
+{
+	clock_control_subsys_t subsys = stm32_get_port_clock(port);
+	/* enable port clock */
+	struct device *clk =
+		device_get_binding(STM32_CLOCK_CONTROL_NAME);
+
+	return clock_control_on(clk, subsys);
+}
+
+static uint32_t pinmux_stm32_set(struct device *dev,
+				 uint32_t pin, uint32_t func)
+{
+	int config;
+
+	/* make sure to enable port clock first */
+	if (enable_port(STM32_PORT(pin)) != DEV_OK) {
+		return DEV_FAIL;
+	}
+
+	/* determine config for alternat function */
+	config = stm32_get_pin_config(pin, func);
+
+	return stm32_pin_configure(pin, config);
+}
+
+static uint32_t pinmux_stm32_get(struct device *dev,
+				 uint32_t pin, uint32_t *func)
+{
+	return DEV_NO_SUPPORT;
+}
+
+static uint32_t pinmux_stm32_input(struct device *dev,
+				uint32_t pin,
+				uint8_t func)
+{
+	return DEV_NO_SUPPORT;
+}
+
+static uint32_t pinmux_stm32_pullup(struct device *dev,
+				uint32_t pin,
+				uint8_t func)
+{
+	return DEV_NO_SUPPORT;
+}
+
+static struct pinmux_driver_api pinmux_stm32_api = {
+	.set = pinmux_stm32_set,
+	.get = pinmux_stm32_get,
+	.pullup = pinmux_stm32_pullup,
+	.input = pinmux_stm32_input,
+};
+
+int pinmux_stm32_init(struct device *port)
+{
+	port->driver_api = &pinmux_stm32_api;
+	return DEV_OK;
+}
+
+static struct pinmux_config pinmux_stm32_cfg = {
+#ifdef CONFIG_SOC_STM32F1X
+	.base_address = GPIO_PORTS_BASE,
+#endif
+};
+
+/**
+ * @brief device init
+ *
+ * Device priority set to 2, so that we come after clock_control and
+ * before any other devices
+ */
+DEVICE_INIT(pinmux_stm32, STM32_PINMUX_NAME, &pinmux_stm32_init,
+	    NULL, &pinmux_stm32_cfg,
+	    PRIMARY, CONFIG_PINMUX_STM32_DEVICE_INITIALIZATION_PRIORITY);
